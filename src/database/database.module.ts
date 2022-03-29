63563020e369f6053f30c51588a582694ae9b078
DaniDani <dani.vendor.sapti@pegadaian.co.id>
2022-03-29 22:01:40 +0700

    add config

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/products/product.entity';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: '127.0.0.1',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest',
-      entities: [Product],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [Product],
+        synchronize: true,
+        dropSchema: true,
+      }),
+      inject: [ConfigService],
     }),
   ],
 })
